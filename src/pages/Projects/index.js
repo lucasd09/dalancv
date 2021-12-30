8777feca90428dba2f0134a2afe9367901200104
lucas dalan <75147965+lucasd09@users.noreply.github.com>
2021-12-30 13:40:35 -0300

    v0.7

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -12,7 +12,7 @@ export default function Projects() {
                 <h3>Projetos que já fiz ou estou fazendo</h3>
                 <Grid container md={12} className='botao-grupo'>
                     <Grid item md={4}><CardProject title='Portfolio Online' text='Desenvolvido em react, um portfolio simples mostrando meu trabalho como desenvolvedor' imgPath={portfolio} wdt={345} link='https://github.com/lucasd09/dalancv'/></Grid>
-                    <Grid item md={4}><CardProject title='Projeto B' text='Em processo de desenvolvmento. Envolve React, Solidity, Tecnologia Blockchain e mais' imgPath={imagem} wdt={345}/></Grid>
+                    <Grid item md={4}><CardProject title='Projeto B' text='Em processo de desenvolvmento. Envolve React, Solidity, Tecnologia Blockchain e mais' imgPath={imagem} wdt={345} link='https://github.com/lucasd09/study-solidity'/></Grid>
                     <Grid item md={4}><CardProject title='W.I.P' text='Work in Progress' imgPath={imagem} wdt={345}/></Grid>
                 </Grid>
             </Container>
